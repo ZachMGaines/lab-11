9a243c889e057ddc37247c6a64614e27d31372b3
Zachary Gaines <zacharygaines28@gmail.com>
2021-05-10 16:51:20 -0700

    rendered mortal kombat characters

---
+++ src/app/characters/CharacterItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './CharacterItem.css';
+
+export default class CharacterItem extends Component {
+  render() {
+    const character = this.props.character;
+
+    return (
+      <li>
+        <Link to={`/characters/${character.id}`}>
+          <h2>{character.name}</h2>
+          <img src={character.url} alt={character.name} />
+          <p>Fighting Style: {character.fightingStyle}</p>
+        </Link>
+      </li>
+    );
+  }
+}
